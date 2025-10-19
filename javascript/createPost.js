ac8870804dfd51a03eff3350ba681b351bf7fe19
JI1047 <kjs0010046@gmail.com>
2025-10-19 16:17:24 +0900

    게시물 작성 페이지 fetch 연결

--- javascript/createPost.js
+++ javascript/createPost.js
@@ -3,6 +3,38 @@ document.getElementById("title").addEventListener("input", (e) => {
   validateEmail(title);
 });
 
+document.getElementById("createPostButton").addEventListener("click", async() => {
+
+    const title = document.getElementById("title").value;
+    const text = document.getElementById("text").value;
+    const profileImage = "www.s3.url"
+
+    const requestBody = {
+        
+        title,
+        text,
+        profileImage
+
+    }
+    try{
+      const response = await fetch("http://127.0.0.1:8080/api/posts/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(requestBody),
+      credentials : "include"
+    });
+
+    if (response.ok) {
+      alert("게시물 생성 성공!");
+      location.href = "/html/getPostList.html";
+
+    } 
+  } catch (error) {
+    alert("서버 요청 중 오류가 발생했습니다.");
+  }
+});
 function validateEmail(title){
     const errorElement =document.getElementById("titleError");
    if(title.length > 26){
