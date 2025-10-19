d5b29918a585452d27e0224bc30d70db3fbb3078
JI1047 <kjs0010046@gmail.com>
2025-10-19 19:35:35 +0900

    게시물 상세 조회 js 주석 추가

--- javascript/getPost.js
+++ javascript/getPost.js
@@ -1,3 +1,13 @@
+/**
+ *  게시물 상세 조회 시 fetch 연결 요청
+ * 1. 게시물 상세 조회 페이지에 접근했을 때 실행되는 fetch 요청
+ * 2. URL의 쿼리 파라미터(id)를 추출하여 postId 변수에 저장
+ * 3,  http://127.0.0.1:8080/api/posts/${postId}로 백엔드 GET요청을 보냄
+ * 3. 세션 쿠키를 포함하기 위헤 credentials: "include"로 설정
+ * 4. 서버로부터 받은 게시물 정보를 HTML 요소(id=title, id=text 등등)에 표시
+ * 5. updatePostButton id로 설정된 게시물 수정 버튼을 클릭 했을 때 이동할 수 있도록 이벤트 리스너 생성
+ * 5. 응답 오류 발생 시 경고창 또는 콘솔을 통해 오류 메시지를 표시
+ */
 document.addEventListener("DOMContentLoaded", async () => {
     
     const urlParam = new URLSearchParams(window.location.search);
