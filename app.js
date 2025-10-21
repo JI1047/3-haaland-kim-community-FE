ddf575d5d886fcfcc90d1f9f196e33e9bcffaffb
JI1047 <kjs0010046@gmail.com>
2025-10-21 19:52:08 +0900

    express 적용/ gitignore파일 생성/헤더(아무 말 대잔치) 링크 이동 추가

---
+++ app.js
@@ -0,0 +1,39 @@
+// node_modules 에 있는 express 관련 파일을 가져온다.
+var express = require('express')
+const path = require('path')
+
+// express 는 함수이므로, 반환값을 변수에 저장한다.
+var app = express()
+
+// 3000 포트로 서버 오픈
+app.listen(3000, function() {
+    console.log("start! express server on port 3000")
+})
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + "/public/html/getPostList.html")
+})
+
+app.get('/createPost',(req,res) =>{
+    res.sendFile(path.join(__dirname, "public/html/createPost.html"))
+})
+app.get('/login',(req,res) =>{
+    res.sendFile(path.join(__dirname, "public/html/login.html"))
+})
+app.get('/signup',(req,res) =>{
+    res.sendFile(path.join(__dirname, "public/html/signup.html"))
+})
+app.get('/getUser',(req,res) =>{
+    res.sendFile(path.join(__dirname, "public/html/getUser.html"))
+})
+app.get('/updateUserProfile',(req,res) =>{
+    res.sendFile(path.join(__dirname, "public/html/updateUserProfile.html"))
+})
+app.get('/updateUserPassword',(req,res) =>{
+    res.sendFile(path.join(__dirname, "public/html/updateUserPassword.html"))
+})
+app.get('/getPostList',(req,res) =>{
+    res.sendFile(path.join(__dirname, "public/html/getPostList.html"))
+})
