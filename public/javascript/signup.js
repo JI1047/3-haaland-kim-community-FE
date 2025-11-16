3f78e6f263fa6963a505450796e3b72e35daf315
JI1047 <kjs001004@gs.anyang.ac.kr>
2025-11-16 17:33:21 +0900

    s3 설정 변경

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -28,8 +28,7 @@ profileFileInput.addEventListener("change", async (e) => {
     //  2) Presigned URL로 직접 업로드 (브라우저 → S3)
     const uploadRes = await fetch(url, {
       method: "PUT",
-      headers: { "Content-Type": file.type, 
-           "x-amz-acl": "public-read"   
+      headers: { "Content-Type": file.type 
       },
       body: file,
     });
